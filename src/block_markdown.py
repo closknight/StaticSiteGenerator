d2f711a98d3d0432d43c0ea229caa561e72edc26
Carlos Caballero <carlos8caballero@gmail.com>
2024-06-13 02:13:00 -0700

    finish markdown to html

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,6 @@
-import re
+from htmlnode import LeafNode, ParentNode
+from inline_markdown import text_to_textnodes
+from textnode import text_node_to_html_node
 
 block_type_paragraph = "paragraph"
 block_type_heading = "heading"
@@ -30,29 +32,108 @@ def block_to_block_type(block: str):
         return block_type_code
     
     if block.startswith(">"):
-        for line in block.split("\n"):
+        for line in lines:
             if not line.startswith(">"):
                 return block_type_paragraph
         return block_type_quote
     
     if block.startswith("* "):
-        for line in block.split("\n"):
+        for line in lines:
             if not line.startswith("* "):
                 return block_type_paragraph
         return block_type_unordered_list
     
     if block.startswith("- "):
-        for line in block.split("\n"):
+        for line in lines:
             if not line.startswith("- "):
                 return block_type_paragraph
         return block_type_unordered_list
     
     if block.startswith("1. "):
         i = 1
-        for line in block.split("\n"):
+        for line in lines:
             if not line.startswith(f"{i}. "):
                 return block_type_paragraph
             i += 1
         return block_type_ordered_list
     
     return block_type_paragraph
+
+def text_to_html(text: str):
+    textnodes = text_to_textnodes(text)
+    children = list(map(text_node_to_html_node, textnodes))
+    return children
+
+def heading_block_to_html(block: str):
+    i = 0
+    for ch in block:
+        if ch != "#" or i > 6:
+            break
+        i += 1
+    if i > 6:
+        raise Exception("Heading level greater than 6")
+    children = text_to_html(block[i + 1:])
+    return ParentNode(f"h{i}", children)
+
+def p_block_to_html(block: str):
+    lines = block.split("\n")
+    text = " ".join(lines)
+    children = text_to_html(text)
+    return ParentNode("p", children)
+
+def quote_block_to_html(block: str):
+    lines = block.split("\n")
+    striped_lines = list(map(lambda line: line.lstrip("> "), lines))
+    text = " ".join(striped_lines)
+    children = text_to_html(text)
+    return ParentNode("blockquote", children)
+
+def code_block_to_html(block: str):
+    block = block[3: -4]
+    children = text_to_html(block)
+    code_block = ParentNode("code", children)
+    return ParentNode("pre", [code_block])
+
+def ul_block_to_html(block: str):
+    def process_li(line: str):
+        line = line.lstrip("*- ")
+        li_children = text_to_html(line)
+        return ParentNode("li", li_children)
+     
+    lines = block.split("\n")
+    list_items = list(map(process_li, lines))
+    return ParentNode("ul", list_items)
+
+def ol_block_to_html(block: str):
+    def process_li(line: str):
+        index = line.find(". ")
+        li_children = text_to_html(line[index + 2:])
+        return ParentNode("li", li_children)
+
+    lines = block.split("\n")
+    list_items = list(map(process_li, lines))
+    return ParentNode("ol", list_items)
+
+def markdown_to_html_node(markdown: str):
+    children = []
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        if block_type == block_type_paragraph:
+            children.append(p_block_to_html(block))
+        elif block_type == block_type_heading:
+            children.append(heading_block_to_html(block))
+        elif block_type == block_type_quote:
+            children.append(quote_block_to_html(block))
+        elif block_type == block_type_code:
+            children.append(code_block_to_html(block))
+        elif block_type == block_type_unordered_list:
+            children.append(ul_block_to_html(block))
+        elif block_type == block_type_ordered_list:
+            children.append(ol_block_to_html(block))
+        else:
+            raise ValueError(f"{block_type} block type does not exist")
+
+    return ParentNode("div", children)
+
+
--- src/textnode.py
+++ src/textnode.py
@@ -20,16 +20,16 @@ class TextNode():
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
     
 def text_node_to_html_node(text_node: TextNode):
-    if text_node.type == text_type_text:
+    if text_node.text_type == text_type_text:
         return LeafNode(None, text_node.text)
-    if text_node.type == text_type_bold:
+    if text_node.text_type == text_type_bold:
         return LeafNode('b', text_node.text)
-    if text_node.type == text_type_italic:
+    if text_node.text_type == text_type_italic:
         return LeafNode('i', text_node.text)
-    if text_node.type == text_type_code:
+    if text_node.text_type == text_type_code:
         return LeafNode('code', text_node.text)
-    if text_node.type == text_type_link:
+    if text_node.text_type == text_type_link:
         return LeafNode('a', text_node.text, {"href": text_node.url})
-    if text_node.type == text_type_image:
+    if text_node.text_type == text_type_image:
         return LeafNode('img', "", {"src": text_node.url, "alt": text_node.text})
     raise ValueError("text node type does not exist")
